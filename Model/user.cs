dc35003475aa11e7db9c96f35a4494b48e8825dc
youcanyouupgithub <469470971@qq.com>
2017-07-01 16:53:21 +0800

    添加实体

---
+++ Model/user.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 系统管理员表
+    /// </summary>
+    public class user
+    {
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int userid { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string username { get; set; }
+
+        /// <summary>
+        /// 用户密码
+        /// </summary>
+        public string password { get; set; }
+
+        /// <summary>
+        /// 作业区ID
+        /// </summary>
+        public int areaid { get; set; }
+
+        /// <summary>
+        /// 班组ID
+        /// </summary>
+        public int classinfoid { get; set; }
+
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime updatetime { get; set; }
+    }
+}
